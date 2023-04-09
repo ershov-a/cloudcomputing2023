b368824eedbf0e8d42b3191ae121c93a32447ecf
Alexander Ershov <5314011+ershov-a@users.noreply.github.com>
2023-04-09 21:22:01 +0300

    Add RSS and GPT

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ from pyowm.owm import OWM
 from telebot import types
 from telebot.types import ReplyKeyboardRemove
 import mysql.connector
+import feedparser
+import openai
+from openai.error import RateLimitError, InvalidRequestError
+from requests.exceptions import ReadTimeout
 
 # Tokens
 with open(os.path.dirname(os.path.realpath(__file__)) + '/.appenv') as file:
@@ -12,9 +16,13 @@ with open(os.path.dirname(os.path.realpath(__file__)) + '/.appenv') as file:
     OWM_TOKEN = file.readline().strip()
     FEEDBACK_GROUP_ID = file.readline().strip()
     MYSQL_ROOT_PASSWORD = file.readline().strip()
+    GPT_TOKEN = file.readline().strip()
 
 # Telegram bot initialization
 bot = telebot.TeleBot(API_TOKEN)
+
+# GPT key
+openai.api_key = GPT_TOKEN
 
 # Logging config
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename="../log.txt", level=logging.DEBUG,
@@ -42,10 +50,50 @@ def send_welcome(message):
 
 # Handle '/currencies', '/news', '/brief', '/feedback'
 # with mock answer
-@bot.message_handler(commands=['currencies', 'news', 'brief'])
+@bot.message_handler(commands=['currencies', 'brief'])
 def mock_message(message):
     update_stats("mock")
     bot.reply_to(message, "Feature in development, try again later")
+
+
+# Handle '/news'
+@bot.message_handler(commands=['news'])
+def news_message(message):
+    update_stats("news")
+
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    button_cnews = types.KeyboardButton(text="CNews")
+    keyboard.add(button_cnews)
+    button_habr = types.KeyboardButton(text="Habr")
+    keyboard.add(button_habr)
+    button_opennet = types.KeyboardButton(text="OpenNET")
+    keyboard.add(button_opennet)
+
+    msg = bot.send_message(message.chat.id, "Select news source", reply_markup=keyboard)
+    bot.register_next_step_handler(msg, news_handle)
+
+
+def news_handle(message):
+    match message.text:
+        case "CNews":
+            news_feed = feedparser.parse("https://www.cnews.ru/inc/rss/news_top.xml")
+            bot.reply_to(message, get_news(news_feed), reply_markup=ReplyKeyboardRemove())
+        case "Habr":
+            news_feed = feedparser.parse("https://habr.com/ru/rss/news/?fl=ru")
+            bot.reply_to(message, get_news(news_feed), reply_markup=ReplyKeyboardRemove())
+        case "OpenNET":
+            news_feed = feedparser.parse("https://www.opennet.ru/opennews/opennews_6_noadv.rss")
+            bot.reply_to(message, get_news(news_feed), reply_markup=ReplyKeyboardRemove())
+        case _:
+            bot.reply_to(message, "Sorry, wrong news feed selected. Try again", reply_markup=ReplyKeyboardRemove())
+
+
+def get_news(news_feed):
+    news = 'Last 10 news: \n'
+    for i in range(10):
+        entry = news_feed.entries[i]
+        news += '\n- ' + entry.title + '\n' + entry.link + '\n'
+    return news
 
 
 # Handle '/weather'
@@ -82,6 +130,56 @@ def weather_handle(message):
         bot.reply_to(message, reply_str, reply_markup=ReplyKeyboardRemove())
     else:
         bot.reply_to(message, "Sorry, this is not a location. Try again", reply_markup=ReplyKeyboardRemove())
+
+
+# Handle '/gpt'
+def gpt_check_length(answer, list_of_answers):
+    if 4090 < len(answer) < 409000:
+        list_of_answers.append(answer[0:4090] + "...")
+        gpt_check_length(answer[4091:], list_of_answers)
+    else:
+        list_of_answers.append(answer[0:])
+        return list_of_answers
+
+
+def gpt_make_request(message):
+    try:
+        engine = "text-davinci-003"
+        completion = openai.Completion.create(
+            engine=engine,
+            prompt=message.text,
+            temperature=0.5,
+            max_tokens=3100,
+        )
+        list_of_answers = gpt_check_length(completion.choices[0]["text"], [])
+        if list_of_answers:
+            for piece_of_answer in list_of_answers:
+                bot.send_message(message.chat.id, piece_of_answer)
+        else:
+            gpt_make_request(message)
+    except (RateLimitError, ReadTimeout):
+        bot.send_message(
+            message.chat.id,
+            "ChatGPT is overloaded, try again later",
+        )
+    except InvalidRequestError:
+        bot.send_message(
+            message.chat.id,
+            "The maximum length of the context 3000 words, the answer exceeded the length of the context. Repeat the "
+            "question or rephrase it",
+        )
+
+
+@bot.message_handler(commands=['gpt'])
+def gpt_message(message):
+    update_stats("gpt")
+
+    msg = bot.send_message(message.chat.id, "Send message for ChatGPT")
+    bot.register_next_step_handler(msg, gpt_handle)
+
+
+def gpt_handle(message):
+    gpt_make_request(message)
 
 
 @bot.message_handler(commands=['feedback'])
--- src/db/init.sql
+++ src/db/init.sql
@@ -7,6 +7,7 @@ CREATE TABLE `stats` (
   `usecount` INT
 );
 INSERT INTO stats (command, usecount) VALUES ('weather', 0);
+INSERT INTO stats (command, usecount) VALUES ('gpt', 0);
 INSERT INTO stats (command, usecount) VALUES ('currencies', 0);
 INSERT INTO stats (command, usecount) VALUES ('news', 0);
 INSERT INTO stats (command, usecount) VALUES ('brief', 0);
